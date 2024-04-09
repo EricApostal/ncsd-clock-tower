b3fe9ed09e7cdc7f51fec6500c8b2483f86f62ad
EricApostal <ehapostal04@gmail.com>
2024-04-09 15:56:48 -0400

    works + menu, some issues when running under a service

--- ncssm_clock.py
+++ ncssm_clock.py
@@ -1,13 +1,19 @@
 import RPi.GPIO as GPIO
-import time, serial, threading, atexit
-import sys
+import time, serial, threading, atexit, sys, os
 
 ser = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0042_85734323430351600190-if00', 9600, timeout=1)
 ser.reset_input_buffer()
 
+close_threads = False
+
+GPIO.setwarnings(False)
+
 # The light value that will signal a change in state
 light_threshold = 300
 current_step = 0
+
+# this should be 60, but it might be nice to set lower for testing
+minute_seconds = 10
 
 current_state = None
 
@@ -18,6 +24,9 @@ def serial_thread():
     global current_state
 
     while True:
+        if(close_threads):
+            sys.exit()
+
         if ser.in_waiting > 0:
             try:
                 light_reading = ser.readline().decode('utf-8').rstrip()
@@ -38,22 +47,22 @@ def exit_handler():
     print("Cleaning up...")
     GPIO.cleanup()
 
-def init_last_state_file():
+def init_last_step_file():
     try:
-        with open("/home/pi/Documents/last_state.txt", "r") as f:
-            print("Last state file already exists")
+        with open("/home/pi/Documents/last_step.txt", "r") as f:
+            print("Last step file already exists")
             return
     except FileNotFoundError:
-        with open("/home/pi/Documents/last_state.txt", "w") as f:
-            print("Last state file created")
+        with open("/home/pi/Documents/last_step.txt", "w") as f:
+            print("Last step file created")
             f.write("0")
 
-def write_last_state(state: int):
-    with open("/home/pi/Documents/last_state.txt", "w") as f:
+def write_last_step(state: int):
+    with open("/home/pi/Documents/last_step.txt", "w") as f:
         f.write(str(state))
 
-def read_last_state():
-    with open("/home/pi/Documents/last_state.txt", "r") as f:
+def read_last_step():
+    with open("/home/pi/Documents/last_step.txt", "r") as f:
         return int(f.read())
 
 """
@@ -71,15 +80,20 @@ def move_steps(steps: int):
         last_state = current_state
         while (current_state == last_state):
             GPIO.output(motor, GPIO.HIGH)
+            time.sleep(0.01)
         current_step += 1
+        print("stepped")
 
         # 720 is the number of steps in 12 hours
         # Once we reach 720, we're back at 12:00
         if (current_step == 720):
             current_step = 0
-        write_last_state(current_state)
 
-        GPIO.output(motor, GPIO.LOW)
+        print("writing last step")
+        write_last_step(current_step)
+        print("wrote last step")
+
+    GPIO.output(motor, GPIO.LOW)
 
 def _sanitize_minute(minute: int):
     if (minute >= 720):
@@ -89,14 +103,8 @@ def _sanitize_minute(minute: int):
 def _calculate_steps(current_minute: int, target_minute: int):
     steps = (target_minute - current_minute) * 2
     if (steps < 0):
-        steps = 720 + steps
+        steps = 1440 + steps
     return steps
-
-def _set_current_step(step: int):
-    global current_step
-    current_step = step
-    if (current_step >= 720):
-        current_step = current_step - 720
 
 def initialize_position():
     global current_step
@@ -112,40 +120,41 @@ def initialize_position():
     if (current_minute_of_day > 720):
         current_minute_of_day -= 720
 
-    current_clock_minute = str(input("[USER]: "))
+    current_clock_minute = str(input("    [USER]: "))
     current_clock_minute = _sanitize_minute(int(current_clock_minute.split(":")[0]) * 60 + int(current_clock_minute.split(":")[1]))
-    # print("Current clock minute: " + str(current_clock_minute))
-    _set_current_step(current_clock_minute * 2)
 
-   #  print("Current step: " + str(current_step))
-    
+    current_step = _sanitize_minute(current_clock_minute * 2)
+
     steps_to_move = _calculate_steps(current_step/2, current_minute_of_day)
-    print("Steps to move: " + str(steps_to_move))
-    # print("Current clock minute: " + str(current_step/2))
-    # print("Target clock minute: " + str(current_clock_minute))
+    print("    [Initialization] Steps to move: " + str(steps_to_move))
 
-    # while current_step != current_clock_minute * 2:
     target_steps = 2
 
     while int(target_steps) > 1:
-        target_steps = _calculate_steps(current_step / 2, current_minute_of_day)
-        print("The clock is not displaying the correct time, moving " + str(target_steps) + " steps to correct it")
+        # print("The clock is not displaying the correct time, moving " + str(target_steps) + " steps to correct it")
         move_steps(target_steps)
+        target_steps = _calculate_steps(current_step / 2, current_minute_of_day)
 
-    print("Clock has been calibrated to " + str(current_clock_minute) + " minutes")
+    new_hour = int(current_minute_of_day / 60)
+    new_minute = current_minute_of_day % 60
+    print("    Clock has been calibrated to " + str(new_hour) + ":" + str(new_minute))
 
 def clock_motor_thread():
+    time.sleep(minute_seconds)
     while True:
         start_time = time.time()
         move_steps(2)
         end_time = time.time()
 
-        diff = 10 - (end_time - start_time)
+        diff = minute_seconds - (end_time - start_time)
 
         if diff > 0:
             time.sleep(diff)
         else:
-            print("Motor took too long to move. Maybe a sensor error?")
+            print("""Motor took too long to move. Maybe a sensor error?
+                  This error is critical! My guess as to what happened is that
+                  the sensor isn't reading properly, so it thinks nothing has moved.
+                  """)
 
 """
 last_state represents the last step
@@ -154,26 +163,79 @@ find the step we are on, and move the motor to that step
 def boot_recalibrate():
     global current_step
 
-    last_state = read_last_state()
+    current_step = read_last_step()
+    print("Last step: " + str(current_step))
     current_minute_of_day = int(time.strftime("%H")) * 60 + int(time.strftime("%M"))
-    current_step = current_minute_of_day * 2
-    step_count = _calculate_steps(last_state, current_step)
-    print("Recalibrating to step: " + str(current_step))
+    target_step = _sanitize_minute(current_minute_of_day) * 2
+    step_count = _calculate_steps(current_step, target_step)
+    print("Recalibrating to step: " + str(target_step) + " with " + str(step_count) + " steps")
     move_steps(step_count)
+
+def spawn_user_menu():
+    os.system('clear')
+    print("""
+    _   _  _____  _____ _____     _____ _      ____   _____ _  __  _______ ______          ________ _____  
+    | \ | |/ ____|/ ____|  __ \   / ____| |    / __ \ / ____| |/ / |__   __/ __ \ \        / /  ____|  __ \ 
+    |  \| | |    | (___ | |  | | | |    | |   | |  | | |    | ' /     | | | |  | \ \  /\  / /| |__  | |__) |
+    | . ` | |     \___ \| |  | | | |    | |   | |  | | |    |  <      | | | |  | |\ \/  \/ / |  __| |  _  / 
+    | |\  | |____ ____) | |__| | | |____| |___| |__| | |____| . \     | | | |__| | \  /\  /  | |____| | \ \ 
+    |_| \_|\_____|_____/|_____/   \_____|______\____/ \_____|_|\_\    |_|  \____/   \/  \/   |______|_|  \_\                                                                                                                                                                                             
+    
+    Powered by NCSSM
+    """)
+    print("""
+    Welcome to the clock control panel. What would you like to do?
+    [1] Calibrate the clock
+    [2] Just start the clock
+          
+    Info:
+    (1) Calibrating the clock will allow you to set the clock to a certain time. You should ideally only have to do this once ever.
+    (2) Starting the clock will just start the clock from the last known position. While you can start it manually, it should
+    already be running right now.
+    """)
+
+    acceptable_input = ["1", "2"]
+    user_input = input("    [USER]: ")
+
+    if user_input not in acceptable_input:
+        print("    Invalid input. Please try again.")
+        user_input = input("    [USER]: ")
+    
+    if user_input == "1":
+        os.system('sudo systemctl stop clock.service')
+        print("    Calibrating...")
+        initialize_position()
+        GPIO.cleanup()
+        os.system('sudo systemctl start clock.service')
+        print("    Clock has been calibrated! You can close this now.")
+        return
+    else:
+        try:
+            os.system('sudo systemctl start clock.service')
+            print("    Started clock service!")
+        except:
+            print("    Error starting clock service. Assuming it's enabled already.")
+        print("    Service is running! You can close this now. Feel free to check on the service by running 'sudo systemctl status clock.service'.")
 
 if __name__ == '__main__':
     if (len(sys.argv) > 2):
         ValueError("Usage: python3 main.py -c (pass -c if you want to callibrate, otherwise leave blank)")
 
     atexit.register(exit_handler)
-    # init_last_state_file()
-    threading.Thread(target=serial_thread).start()
+    init_last_step_file()
+
+    serial_thread = threading.Thread(target=serial_thread)
+    serial_thread.start()
     time.sleep(1)
 
-    if (len(sys.argv) == 2 and sys.argv[1] == "-c"):
-        print("Calibrating...")
-        initialize_position()
+    if (len(sys.argv) == 2):
+        if (sys.argv[1] == "-c"):
+            print("Calibrating...")
+            initialize_position()
+        elif (sys.argv[1] == "-m"):
+            spawn_user_menu()
     else:
         print("Starting clock...")
         boot_recalibrate()
+        print("Clock has been calibrated, running motor listener")
         threading.Thread(target=clock_motor_thread).start()
---
+++ run_calibrate.sh
@@ -0,0 +1,3 @@
+# Create a menu with the following options:
+# 1. Run the command "python3 ncssm_clock.py" (Run program like normal)
+# 2. Run the command "python3 ncssm_clock.py -c"
